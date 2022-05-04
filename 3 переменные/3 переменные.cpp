3749c6836d3a9f9c83f057f65af36bb545d599f9
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-05-04 21:25:24 +0200

    3 variables

---
+++ 3 переменные/3 переменные.cpp
@@ -0,0 +1,38 @@
+﻿// 3 переменные.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a, b, c, d, e;
+
+    // 3 variables
+    cout << "Enter two variables: " << endl;
+
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    cout << "It was: a = " << a << ", b = " << b << endl;
+    c = a;
+    a = b;
+    b = c;
+
+    cout << "It became: a = " << a << ", b = " << b << endl;
+
+    // 2 variabels
+    cout << "Enter two variables: " << endl;
+
+    cout << "d = ";
+    cin >> d;
+    cout << "e = ";
+    cin >> e;
+
+    cout << "It was: d = " << d << ", e = " << e << endl;
+    swap(e, d);
+
+    cout << "It became: d = " << d << ", e = " << e << endl;
+}
